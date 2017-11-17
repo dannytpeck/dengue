c73d04809f4adeec8e089c424c2a789d8be21d55
Danny Peck <dannytpeck@gmail.com>
2017-11-16 19:01:36 -0800

    Move tile preview to its own component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Header from './Header';
+import Header from './header';
+import TilePreview from './tile_preview';
 
 class App extends Component {
   constructor(props) {
@@ -30,36 +31,7 @@ class App extends Component {
           </div>
 
           <div className="col-5 offset-1">
-            <div id="tile">
-              <div className="stretchy-wrapper">
-                <img className="item-info-image" src="https://d1dyf6uqjwvcrk.cloudfront.net/cfs-file.ashx/__key/CommunityServer-Components-PostAttachments/00-19-63-91-59/Ember_5F00_Tile.png" alt="" />
-              </div>
-              <div id="more-info-container">
-                <div className="info-header">
-                  <h3 className="info-title">This is the Title</h3>
-                </div>
-                <div className="item-info-details">
-                  <div className="description-text">
-                    {/* Begin HTML Description */}
-                    <div className="ember-content">
-                      <p><em>Ember</em> is your digital companion. It will enable you to become the best version of yourself.</p>
-                      <h3>Ember allows you to:</h3>
-                      <ul>
-                        <li>Prioritize what matters most to you</li>
-                        <li>Pick your goals and monitor your progress</li>
-                        <li>Access interesting and relevant content + videos</li>
-                        <li>Participate in self-guided learning</li>
-                        <li>Book your screening &amp; coaching appointments on-the-go</li>
-                      </ul>
-                    </div>
-                    {/* End HTML Description */}
-                  </div>
-                </div>
-                <div className="item-info-actions">
-                  <button className="button button-primary">Close</button>
-                </div>
-              </div>
-            </div>
+            <TilePreview />
           </div>
         </div>
 
