d719749cd5254eaeaac25abe2904d6e153c69d87
Danny Peck <dannytpeck@gmail.com>
2017-11-16 19:49:56 -0800

    Hook up tile preview to data

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,9 +2,73 @@ import React, { Component } from 'react';
 import Header from './header';
 import TilePreview from './tile_preview';
 
+/* global $ */
+
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      imageSrc: 'https://d1dyf6uqjwvcrk.cloudfront.net/cfs-file.ashx/__key/CommunityServer-Components-PostAttachments/00-19-63-91-59/Ember_5F00_Tile.png',
+      title: 'Your mobile experience is here',
+      description: '<div style="font-size: 14px; line-height: 1.3em"><div class="ember-content" style="padding-bottom: 20px;"> <p><em>Ember</em> is your digital companion. It will enable you to become the best version of yourself.</p> <h3>Ember allows you to:</h3> <ul> <li>Prioritize what matters most to you</li> <li>Pick your goals and monitor your progress</li> <li>Access interesting and relevant content + videos</li> <li>Participate in self-guided learning</li> <li>Book your screening &amp; coaching appointments on-the-go</li></ul></div><div class="app-store-container" style="position: relative; height: 80px; padding: 0 0; margin-bottom: 0;"> <div class="app-store-icon ios-download-icon" style="width: 50%; height: 100%; display: inline-block; float: left; padding-top: 16px;"> <a href="" style="margin: 0 0 0 48px;"><img src="https://mywellnessnumbers.com/HumanPerformance/images/ios-download-icon.png" alt="Download ember on the iOS App Store"></a> </div> <div class="app-store-icon android-download-icon" style="width: 50%; height: 100%; display: inline-block; float: left; padding-top: 16px;"> <a href="" style="margin: 0 48px 0 16px;"><img src="https://mywellnessnumbers.com/HumanPerformance/images/android-download-icon.png" alt="Download ember on the Google Play Store"></a></div></div></div>'
+    };
+
+    this.getToken = this.getToken.bind(this);
+    this.getEventData = this.getEventData.bind(this);
+  }
+
+  getEventData(eventId, token) {
+    const url = `https://api.limeade.com/api/activity/${eventId}/Get?types=5&status=2&attributes=1&contents=32319`;
+    const headers = {
+      Authorization: `Bearer ${token}`
+    };
+
+    $.ajax(url, {
+      type: 'get',
+      headers: headers,
+      dataType: 'json',
+      success: (data) => {
+        console.log(data);
+        const event = data.Data[0];
+
+        this.setState({
+          imageSrc: event.MediumImageSrc,
+          title: event.Title,
+          description: event.HtmlDescription
+        });
+      }
+    });
+  }
+
+  getToken(e) {
+    e.preventDefault();
+
+    const headers = {
+      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IlNTZ2w4Zzg1ZDNELUlVaFY3dXB5bkQzMEVYTSIsImtpZCI6IlNTZ2w4Zzg1ZDNELUlVaFY3dXB5bkQzMEVYTSJ9.eyJjbGllbnRfaWQiOiJpbnRlcm5hbGNsaWVudCIsInNjb3BlIjpbImFwaWFjY2VzcyIsIm9wZW5pZCIsInBpaWlkZW50aXR5Il0sInN1YiI6IjM3NzY0NzEiLCJhbXIiOiJwYXNzd29yZCIsImF1dGhfdGltZSI6MTUwMDQ5MzM5NiwiaWRwIjoiaWRzcnYiLCJuYW1lIjoib21lbGV0dGUiLCJlbXBsb3llcmlkIjoiMTAwODQ1Iiwicm9sZSI6IlVzZXIiLCJlbXBsb3llcm5hbWUiOiJXZWxsbWV0cmljc0RlbW8iLCJlbWFpbCI6Impvc2VwaC5wYWtAYWR1cm9saWZlLmNvbSIsImdpdmVuX25hbWUiOiJKb3NlcGgiLCJmYW1pbHlfbmFtZSI6IlBhayIsImlzcyI6Ind3dy5saW1lYWRlLmNvbSIsImF1ZCI6Ind3dy5saW1lYWRlLmNvbS9yZXNvdXJjZXMiLCJleHAiOjE1MzIwMjkzOTYsIm5iZiI6MTUwMDQ5MzM5Nn0.Yp6t1W9o8oJWqUGXtjr6fVwnRi3kdann3MJVXBPHoRB_AAnniRbbUkZbggh6fBgDvRvlDOs029K9vkNJ1DehyAO1cwZK0LibPoKvHy1Y65GiK-LoTQHalTMYryuK76GhQ-sxu8pA7JwRS0p0_LxRhY8-pf7gORAawhXwCZ4wz_sClyWsPn6DnqYEWEkEfyIEvT88hyClnZGILuDjv12qIm2EqXrGYLG6JeZzlnj7z915MXHPWFLywin7_Ff7CYcbbnIce0aQts26P0R2srbjtxiIeJBBxuq_StS-HMexzz5p0gFihlPxhFVLsb7OYiubPXBjPQpCqm85b7NrVQUwaw',
+      'Content-Type': 'application/json'
+    };
+
+    const employerName = $('#employerName').val();
+    const password = $('#password').val();
+    const eventId = $('#eventId').val();
+
+    const data = {
+      Username: employerName,
+      Password: password
+    };
+
+    $.ajax({
+      url: 'https://api.limeade.com/api/usertoken',
+      type: 'post',
+      headers: headers,
+      data: JSON.stringify(data),
+      dataType: 'json',
+      success: (data) => {
+        console.log(data);
+        this.getEventData(eventId, data.access_token);
+      }
+    });
   }
 
   render() {
@@ -27,11 +91,12 @@ class App extends Component {
                 <label htmlFor="eventId">Event ID</label>
                 <input type="text" className="form-control" id="eventId" placeholder="2350" />
               </div>
+              <button type="submit" className="btn btn-primary" onClick={this.getToken}>Submit</button>
             </form>
           </div>
 
           <div className="col-5 offset-1">
-            <TilePreview />
+            <TilePreview imageSrc={this.state.imageSrc} title={this.state.title} description={this.state.description} />
           </div>
         </div>
 
--- src/components/tile_preview.jsx
+++ src/components/tile_preview.jsx
@@ -1,32 +1,22 @@
 import React, { Component } from 'react';
 
 class TilePreview extends Component {
+  createHtml() {
+    return { __html: this.props.description };
+  }
+
   render() {
     return (
       <div id="tile-preview">
         <div className="stretchy-wrapper">
-          <img className="item-info-image" src="https://d1dyf6uqjwvcrk.cloudfront.net/cfs-file.ashx/__key/CommunityServer-Components-PostAttachments/00-19-63-91-59/Ember_5F00_Tile.png" alt="" />
+          <img className="item-info-image" src={this.props.imageSrc} alt="" />
         </div>
         <div id="more-info-container">
           <div className="info-header">
-            <h3 className="info-title">This is the Title</h3>
+            <h3 className="info-title">{this.props.title}</h3>
           </div>
           <div className="item-info-details">
-            <div className="description-text">
-              {/* Begin HTML Description */}
-              <div className="ember-content">
-                <p><em>Ember</em> is your digital companion. It will enable you to become the best version of yourself.</p>
-                <h3>Ember allows you to:</h3>
-                <ul>
-                  <li>Prioritize what matters most to you</li>
-                  <li>Pick your goals and monitor your progress</li>
-                  <li>Access interesting and relevant content + videos</li>
-                  <li>Participate in self-guided learning</li>
-                  <li>Book your screening &amp; coaching appointments on-the-go</li>
-                </ul>
-              </div>
-              {/* End HTML Description */}
-            </div>
+            <div className="description-text" dangerouslySetInnerHTML={this.createHtml()}></div>
           </div>
           <div className="item-info-actions">
             <button className="button button-primary">Close</button>
