3cd25060f1935a8bea4e910f09e03d2bbc2fec4f
Danny Peck <dannytpeck@gmail.com>
2018-01-19 14:50:36 -0800

    Add functionality to upload changes

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -10,59 +10,19 @@ class App extends Component {
 
     this.state = {
       hasLoaded: false,
-      imageSrc: 'https://limeade.com/cfs-file.ashx/__key/CommunityServer-Components-PostAttachments/00-19-63-91-59/Ember_5F00_Tile.png',
+      imageSrc: 'https://mywellmetrics.com/cfs-file.ashx/__key/CommunityServer-Components-PostAttachments/00-19-63-91-59/Ember_5F00_Tile.png',
       title: 'Your mobile experience is here',
       description: '<div style="font-size: 14px; line-height: 1.3em"><div class="ember-content" style="padding-bottom: 20px;"><p><em>Ember</em> is your digital companion. It will enable you to become the best version of yourself.</p><h3>Ember allows you to:</h3><ul><li>Prioritize what matters most to you</li><li>Pick your goals and monitor your progress</li><li>Access interesting and relevant content + videos</li><li>Participate in self-guided learning</li><li>Book your screening &amp; coaching appointments on-the-go</li></ul></div><div class="app-store-container" style="position: relative; height: 80px; padding: 0 0; margin-bottom: 0;"><div class="app-store-icon ios-download-icon" style="width: 50%; height: 100%; display: inline-block; float: left; padding-top: 16px;"><a href="" style="margin: 0 0 0 48px;"><img src="https://mywellnessnumbers.com/HumanPerformance/images/ios-download-icon.png" alt="Download ember on the iOS App Store"></a></div><div class="app-store-icon android-download-icon" style="width: 50%; height: 100%; display: inline-block; float: left; padding-top: 16px;"><a href="" style="margin: 0 48px 0 16px;"><img src="https://mywellnessnumbers.com/HumanPerformance/images/android-download-icon.png" alt="Download ember on the Google Play Store"></a></div></div></div>',
       points: '100',
+      maxOccurrences: '',
       displayPriority: '',
-      targeting: '',
-      maxOccurrences: ''
+      targeting: ''
     };
 
     this.getToken = this.getToken.bind(this);
     this.getInitialData = this.getInitialData.bind(this);
     this.getEventData = this.getEventData.bind(this);
-  }
-
-  getEventData(eventId, headers, imageUrl, maxOccurrences) {
-    const url = `https://api.limeade.com/api/admin/activity/-${eventId}`;
-
-    $.ajax(url, {
-      type: 'get',
-      headers: headers,
-      dataType: 'json',
-      success: (data) => {
-        const event = data.Data;
-        this.setState({
-          imageSrc: imageUrl,
-          title: event.Name,
-          description: event.AboutChallenge,
-          points: event.ActivityReward.Value,
-          displayPriority: event.DisplayPriority,
-          targeting: event.Targeting,
-          maxOccurrences: maxOccurrences,
-          hasLoaded: true
-        });
-      }
-    });
-  }
-
-  getInitialData(eventId, token) {
-    const url = `https://api.limeade.com/api/activity/${eventId}/Get?types=1&status=1&attributes=1&contents=31`;
-    const headers = {
-      Authorization: `Bearer ${token}`
-    };
-    $.ajax(url, {
-      type: 'get',
-      headers: headers,
-      dataType: 'json',
-      success: (data) => {
-        const event = data.Data[0];
-        const imageUrl = event.MediumImageSrc;
-        const maxOccurrences = event.Reward.MaxCount;
-        this.getEventData(eventId, headers, imageUrl, maxOccurrences);
-      }
-    });
+    this.submitData = this.submitData.bind(this);
   }
 
   getToken(e) {
@@ -94,84 +54,82 @@ class App extends Component {
     });
   }
 
-  submitData() {
+  getInitialData(eventId, token) {
+    // Literally just used to get MaxOccurrences and EventImageUrl
+    const url = `https://api.limeade.com/api/activity/${eventId}/Get?types=1&status=1&attributes=1&contents=31`;
+    const headers = {
+      Authorization: `Bearer ${token}`
+    };
+    $.ajax(url, {
+      type: 'get',
+      headers: headers,
+      dataType: 'json',
+      success: (data) => {
+        const event = data.Data[0];
+        const imageUrl = event.MediumImageSrc;
+        const maxOccurrences = event.Reward.MaxCount;
+        this.getEventData(eventId, headers, imageUrl, maxOccurrences);
+      }
+    });
+  }
+
+  getEventData(eventId, headers, imageUrl, maxOccurrences) {
+    // Gets all other data
+    const url = `https://api.limeade.com/api/admin/activity/-${eventId}`;
+
+    $.ajax(url, {
+      type: 'get',
+      headers: headers,
+      dataType: 'json',
+      success: (data) => {
+        const event = data.Data;
+        this.setState({
+          title: event.Name,
+          description: event.AboutChallenge,
+          points: event.ActivityReward.Value,
+          displayPriority: event.DisplayPriority,
+          targeting: event.Targeting,
+          imageSrc: imageUrl,
+          maxOccurrences: maxOccurrences,
+          hasLoaded: true
+        });
+      }
+    });
+  }
+
+  submitData(e) {
+    e.preventDefault();
+
     // Forced to use the old API... ughhh.
-    /*
-    {
-      "Message": "",
-      "Data": [
-        {
-          "Id": 2350,
-          "GoalId": 2350,
-          "Title": "Welcome to Human Performance",
-          "Type": 1,
-          "Frequency": 0,
-          "TargetGoal": 0,
-          "Status": 2,
-          "Attributes": 28,
-          "TrackingMode": 0,
-          "WinStrategy": 0,
-          "TargetUrl": "",
-          "StartDate": "1/1/0001",
-          "EndDate": "12/31/9999",
-          "Reward": {
-            "Value": "0",
-            "MaxCount": 1,
-            "Type": 0
-          },
-          "SupportedDevices": [],
-          "DevicesNeedingReauth": [],
-          "CommentsCount": 0,
-          "ActiveParticipantCount": 0,
-          "Leaderboards": [],
-          "Source": 1,
-          "ShortDescription": "",
-          "HtmlDescription": "<p><strong>What is Human Performance?</strong> <br>Human Performance addresses the interconnected elements of life: <strong>Health &amp; Fitness</strong>, <strong>Money &amp; Prosperity</strong>, <strong>Growth &amp; Development</strong> and <strong>Contribution &amp; Sustainability</strong>. Improving one element makes the others stronger &ndash; for example, improving your finances can lead to better health. Maximizing Human Performance creates the foundation you need for a more fulfilling life.</p><p style=\"margin-top:10px\">Click on any one of the Human Performance elements on your home page to learn why they're important, how they impact the other areas of your life and how you can use our resources to achieve your goals.</p><img src=\"https://challenges.mywellnessnumbers.com/images/HP_AboutTheProgram.png\" alt=\"hp_abouttheprogram\" width=\"100%\" />",
-          "ExtendedProperties": {},
-          "ActivityLog": [],
-          "Progress": null,
-          "Actions": [
-            {
-              "Type": "RedirectToTarget",
-              "Details": {
-                "Text": "Close",
-                "Url": ""
-              },
-              "Text": "Close",
-              "Url": ""
-            }
-          ],
-          "FlagText": 0,
-          "StrategyText": null,
-          "PrivacyFlag": 0,
-          "IsTeamChallenge": false,
-          "TeamSizeMax": 0,
-          "TeamSizeMin": 0,
-          "IsWeightChallenge": false,
-          "ShowWeeklyCalendar": false,
-          "ShowExtendedDescription": false,
-          "LastTrackedDate": null,
-          "MediumImageSrc": "https://d1dyf6uqjwvcrk.cloudfront.net/cfs-file.ashx/__key/CommunityServer-Components-PostAttachments/00-18-07-97-79/BestLife_5F00_Tile.png",
-          "SmallImageSrc": "https://d1dyf6uqjwvcrk.cloudfront.net/cfs-file.ashx/__key/CommunityServer-Components-PostAttachments/00-18-26-11-60/thumb_5F00_BestLife_5F00_5F00_5F00_Tile.png",
-          "IsPHITargeted": false,
-          "IsDSTLeaderboardEnabled": false,
-          "IsPartnerVerifiedChallenge": false,
-          "IsPeerToPeerChallenge": false,
-          "IsLearningChallenge": false
-        }
-      ],
-      "RecordCount": 0,
-      "CreateTime": "2017-11-28T08:56:39"
+    const csv = createCSV();
+    console.log(csv);
+    uploadToLimeade(csv);
+
+    function clean(dirtyText) {
+    // Gets rid of anything we don't want in the final HTML output
+      const cleanText = dirtyText
+        .replace(/\u00A9/g, '&copy;')
+        .replace(/\u2013/g, '-')
+        .replace(/\u2014/g, '-')
+        .replace(/\u2019/g, '\'')
+        .replace(/\u201C/g, '"')
+        .replace(/\u201D/g, '"')
+        .replace(/\t/g, '')
+        .replace('rgb(255, 255, 255)', '#fff')
+        .replace('rgb(255, 255, 255)', '#fff')
+        .replace('rgb(228, 229, 231)', '#e4e5e7')
+        .replace('rgb(61, 98, 116)', '#3d6274');
+
+      return cleanText;
     }
-    */
 
     // Create a CSV for CIE uploads
-    const createCSV = () => {
+    function createCSV() {
       let data = [[
         'EmployerName',
         'EventId',
         'EventName',
-        'DisplayPiority',
+        'DisplayPriority',
         'RewardType',
         'PointsAwarded',
         'RewardDescription',
@@ -201,21 +159,23 @@ class App extends Component {
         'Field3Value'
       ]];
 
-      const eventName = '';
-      const htmlDescription = '';
-      const employerName = '';
-      const eventId = '';
-      const pointsAwarded = '';
-      const eventImageUrl = '';
-      const maxOccurrences = '';
+      const eventName = $('.info-title').html();
+      const htmlDescription = $('.description-text').html();
+      const employerName = $('#employerName').val();
+      const eventId = $('#eventId').val();
+      const pointsAwarded = $('.info-points span').html();
+      const fullImageUrl = $('.item-info-image').attr('src');
+      const eventImageUrl = fullImageUrl.substring(fullImageUrl.indexOf('/cfs'));
+      const maxOccurrences = $('#maxOccurrences').val();
+      const displayPriority = $('#displayPriority').val();
 
       const cie = [
         employerName,
         eventId,
-        '"' + eventName + '"',
-        '',
+        '"' + eventName.replace('&amp;', '&') + '"',
+        displayPriority,
         'IncentivePoints',
-        pointsAwarded,
+        pointsAwarded.replace(',', ''),
         '',
         '0',
         '0',
@@ -233,24 +193,31 @@ class App extends Component {
         '',
         '',
         '',
-        '"' + htmlDescription.replace(/"/g, '""') + '"',
+        '"' + clean(htmlDescription).replace(/"/g, '""') + '"',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        ''
       ];
 
       data.push(cie);
 
       return data;
-    };
+    }
 
-    const uploadToLimeade = (type) => {
-      const csv = createCSV(type);
+    // Upload CSV to Limeade
+    function uploadToLimeade(csv) {
       const headers = csv[0].join(',');
       const url = 'http://mywellnessnumbers.sftp.adurolife.com/limeade-upload/';
 
       const oneIncentiveEvent = csv[1].join(',');
 
       const params = {
-        e: $('#employer-name').val(),
-        psk: $('#limeadePSK').val(),
+        e: $('#employerName').val(),
+        psk: $('#psk').val(),
         data: headers + '\n' + oneIncentiveEvent,
         type: 'IncentiveEvents'
       };
@@ -259,7 +226,7 @@ class App extends Component {
         console.log(response);
       });
 
-    };
+    }
 
   }
 
@@ -270,6 +237,7 @@ class App extends Component {
 
         <div className="row">
           <div className="col-4 offset-1">
+
             <form id="form">
               <div className="form-group">
                 <label htmlFor="employerName">EmployerName</label>
@@ -280,20 +248,43 @@ class App extends Component {
                 <input type="text" className="form-control" id="password" placeholder="CoolSkeleton95" readOnly={this.state.hasLoaded} />
               </div>
               <div className="form-group">
-                <label htmlFor="psk">PSK</label>
-                <input type="text" className="form-control" id="psk" placeholder="ABCD-1234-ABCD-1234" readOnly={this.state.hasLoaded} />
-              </div>
-              <div className="form-group">
                 <label htmlFor="eventId">Event ID</label>
                 <input type="text" className="form-control" id="eventId" placeholder="2350" readOnly={this.state.hasLoaded} />
               </div>
-              <button type="submit" className="btn btn-primary" onClick={this.getToken} disabled={this.state.hasLoaded}>Fetch CIE</button>
+              <div className="form-group">
+                <button type="submit" className="btn btn-primary" onClick={this.getToken} disabled={this.state.hasLoaded}>Fetch CIE</button>
+              </div>
             </form>
+
+            <form id="form">
+              <div className="form-group">
+                <label htmlFor="psk">PSK</label>
+                <input type="text" className="form-control" id="psk" placeholder="ABCD-1234-ABCD-1234" />
+              </div>
+              <div className="form-group">
+                <button type="submit" className="btn btn-primary" onClick={this.submitData}>Upload Changes</button>
+              </div>
+            </form>
+
+            <div className="form-group">
+              <label htmlFor="maxOccurrences">Max Occurrences</label>
+              <input type="text" className="form-control" id="maxOccurrences" placeholder="1" value={this.state.maxOccurrences} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="displayPriority">Display Priority</label>
+              <input type="text" className="form-control" id="displayPriority" value={this.state.displayPriority} />
+            </div>
+            <div className="form-group" id="targetingContainer">
+              <label htmlFor="targeting">Targeting</label>
+              <input type="text" className="form-control" id="targeting" value={this.state.targeting} />
+            </div>
+
           </div>
 
           <div className="col-5 offset-1">
             <TilePreview imageSrc={this.state.imageSrc} title={this.state.title} description={this.state.description} points={this.state.points} />
           </div>
+
         </div>
 
       </div>
