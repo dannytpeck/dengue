3d821f0d5cd705d4b0894ab8cf8252c3d3005e1e
Josh Braun <josh.braun@adurolife.com>
2018-02-23 14:31:04 -0800

    Host and change image src to avoid being a hotlinking d-bag

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -11,7 +11,7 @@ class App extends Component {
     this.state = {
       clients: [],
       hasLoaded: false,
-      imageSrc: 'https://images.template.net/wp-content/uploads/2016/04/Shrug-Emoticon-Japanese-Kaomoji-Download1.jpg',
+      imageSrc: 'http://mywellnessnumbers.sftp.adurolife.com/dengue/images/shrug.png',
       title: 'Your CIE will appear here',
       description: '<div style="font-size: 14px; line-height: 1.3em"><div><p>Here you can write some additional details about how to complete the challenge. You could also add some tips for people at different skill levels for the activity.</p></div><p style="font-size: 9px;"><span>&copy; Copyright 3030 </span><a href="http://www.adurolife.com" target="_blank" style="text-decoration: none;">ADURO, INC.</a><span> All rights reserved.</span></p></div>',
       points: '100',
