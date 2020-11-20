186bbc5e9a472124aeac42979f593f3948f67587
Danny Peck <dannytpeck@gmail.com>
2020-11-20 15:38:33 -0800

    Update to function-style

--- src/components/trumbowyg_box.jsx
+++ src/components/trumbowyg_box.jsx
@@ -1,40 +1,41 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import cleanPastedHTML from '../helpers/clean_pasted_html';
 
-class TrumbowygBox extends Component {
-  constructor(props) {
-    super(props);
-  }
+/* global $ */
+function TrumbowygBox({ text }) {
 
-	componentDidMount() {
-    /* global $ */
-		$('.description-text').trumbowyg(
-			{ btns: 
-				[
-				['viewHTML'],
-        ['undo', 'redo'], // Only supported in Blink browsers
-        ['formatting'],
-        ['strong', 'em', 'del'],
-        ['superscript', 'subscript'],
-        ['link'],
-        ['foreColor', 'backColor'],
-        ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
-        ['unorderedList', 'orderedList'],
-        ['horizontalRule'],
-        ['removeformat']
-				] }
-			).on('tbwpaste', (event) => {
-      event.target.innerHTML = cleanPastedHTML(event.target.innerHTML);
+  // Make airtable calls when app starts
+  useEffect(() => {
+
+    // Enable trumbowyg
+    $(function() {
+      $('.description-text').trumbowyg({
+        btns: [
+          ['viewHTML'],
+          ['undo', 'redo'], // Only supported in Blink browsers
+          ['formatting'],
+          ['strong', 'em', 'del'],
+          ['superscript', 'subscript'],
+          ['link'],
+          ['foreColor', 'backColor'],
+          ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+          ['unorderedList', 'orderedList'],
+          ['horizontalRule'],
+          ['removeformat']
+        ]
+      }).on('tbwpaste', (event) => {
+        event.target.innerHTML = cleanPastedHTML(event.target.innerHTML);
+      });
     });
-	}
 
-	render() {
-		return (
-			<div className="trumbowyg-box">
-				<div className="description-text" dangerouslySetInnerHTML={{ __html: this.props.text }}></div>
-			</div>
-		);
-	}
+  }, []); // Pass empty array to only run once on mount
+
+  return (
+    <div className="trumbowyg-box">
+      <div className="description-text" dangerouslySetInnerHTML={{ __html: text }}></div>
+    </div>
+  );
+
 }
 
 export default TrumbowygBox;
